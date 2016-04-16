1a575b8d2e1e1f58075b732d93c47c91ae08c483
Dan K <itsdanya@gmail.com>
2016-04-16 20:36:00 +0300

    Install VCR

--- omdbapi.gemspec
+++ omdbapi.gemspec
@@ -21,5 +21,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "vcr"
   spec.add_dependency 'httparty', '>= 0.13'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,15 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'omdbapi'
+require 'vcr'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr'
+  config.hook_into :webmock
 end
