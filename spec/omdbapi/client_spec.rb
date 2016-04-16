338b5078ee969834a4f2a2da67cb241da526a263
Dan K <itsdanya@gmail.com>
2016-04-16 20:36:47 +0300

    Use VCR cassettes in client spec

--- spec/omdbapi/client_spec.rb
+++ spec/omdbapi/client_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe OMDB::Client do
-
   it 'should include HTTParty' do
     OMDB::Client.should include(HTTParty)
   end
@@ -11,162 +10,191 @@ describe OMDB::Client do
   end
 
   describe 'methods' do
-
     describe 'title' do
       describe 'with a movie that exists' do
-        let(:title) { OMDB.title('Star Wars') }
+        before(:all) do
+          VCR.use_cassette('title/valid') do
+            @title = OMDB.title('Star Wars')
+          end
+        end
 
         it 'should return a hash of movie attributes' do
-          title.should be_instance_of Hash
+          @title.should be_instance_of Hash
         end
 
         it 'should contain a title' do
-          title.title.should be_instance_of String
+          @title.title.should be_instance_of String
         end
       end
 
       describe 'with the year parameter' do
-        let(:title1) { OMDB.title('True Grit') }
-        let(:title2) { OMDB.title('True Grit', year: '1969') }
-
         it 'should not be the same title' do
-          title1.should_not eq(title2)
+          VCR.use_cassette('title/year') do
+            title1 = OMDB.title('True Grit')
+            title2 = OMDB.title('True Grit', year: '1969')
+            title1.should_not eq(title2)
+          end
         end
       end
 
       describe 'with the plot parameter' do
-        let(:title1) { OMDB.title('Game of Thrones') }
-        let(:title2) { OMDB.title('Game of Thrones', plot: 'full') }
-
         it 'should have different plots' do
-          title1.plot.should_not eq(title2.plot)
+          VCR.use_cassette('title/plot') do
+            title1 = OMDB.title('Game of Thrones')
+            title2 = OMDB.title('Game of Thrones', plot: 'full')
+            title1.plot.should_not eq(title2.plot)
+          end
         end
       end
 
       describe 'with the season and episode parameters' do
-        let(:title) { OMDB.title('True Detective', episode: 1, season: 1) }
-
         it 'should include season and episode in the response' do
-          title.season.should eq("1")
-          title.episode.should eq("1")
+          VCR.use_cassette('title/season_and_episode') do
+            title = OMDB.title('True Detective', episode: 1, season: 1)
+            title.season.should eq('1')
+            title.episode.should eq('1')
+          end
         end
       end
 
       describe 'with only the season parameter, missing the episode parameter' do
-        let(:title) { OMDB.title('True Detective', season: 1) }
-
-        it 'should not include season and episode in the response' do
-          expect { title.season }.to raise_error(NoMethodError)
-          expect { title.episode }.to raise_error(NoMethodError)
+        it 'should not include episode in the response' do
+          VCR.use_cassette('title/season') do
+            title = OMDB.title('True Detective', season: 1)
+            expect { title.episode }.to raise_error(NoMethodError)
+          end
         end
       end
 
       describe 'with only the episode parameter, missing the season parameter' do
-        let(:title) { OMDB.title('True Detective', episode: 1) }
-
         it 'should not include season and episode in the response' do
-          expect { title.season }.to raise_error(NoMethodError)
-          expect { title.episode }.to raise_error(NoMethodError)
+          VCR.use_cassette('title/episode') do
+            title = OMDB.title('True Detective', episode: 1)
+            expect { title.season }.to raise_error(NoMethodError)
+            expect { title.episode }.to raise_error(NoMethodError)
+          end
         end
       end
 
-      describe 'with a movie that doesn''t exist' do
-        let(:title) { OMDB.title('lsdfoweirjrpwef323423dsfkip') }
-        
+      describe "with a movie that doesn't exist" do
+        before(:all) do
+          VCR.use_cassette('title/nonexistent') do
+            @title = OMDB.title('lsdfoweirjrpwef323423dsfkip')
+          end
+        end
+
         it 'should return a hash' do
-          title.should be_instance_of Hash
+          @title.should be_instance_of Hash
         end
 
         it 'should return a hash with a false response' do
-          title.response.should eq('False')
+          @title.response.should eq('False')
         end
 
         it 'should return a hash with an error message' do
-          title.error.should be_instance_of String
+          @title.error.should be_instance_of String
         end
       end
     end
 
     describe 'id' do
-
       describe 'with a title that exists' do
-        let(:title) { OMDB.id('tt0411008') }
+        before(:all) do
+          VCR.use_cassette('id/existing') do
+            @title = OMDB.id('tt0411008')
+          end
+        end
 
         it 'should return a hash of movie attributes' do
-          title.should be_instance_of Hash
+          @title.should be_instance_of Hash
         end
 
         it 'should contain a title' do
-          title.title.should be_instance_of String
+          @title.title.should be_instance_of String
         end
       end
 
-      describe 'with a movie that doesn''t exist' do
-        let(:title) { OMDB.id('tt1231230123') }
-        
+      describe "with a movie that doesn't exist" do
+        before(:all) do
+          VCR.use_cassette('id/nonexistent') do
+            @title = OMDB.id('tt1231230123')
+          end
+        end
+
         it 'should return a hash' do
-          title.should be_instance_of Hash
+          @title.should be_instance_of Hash
         end
 
         it 'should return a hash with a false response' do
-          title.response.should eq('False')
+          @title.response.should eq('False')
         end
 
         it 'should return a hash with an error message' do
-          title.error.should be_instance_of String
+          @title.error.should be_instance_of String
         end
       end
     end
 
     describe 'search' do
-
       describe 'with search results' do
-        let(:results) { OMDB.search('Star Wars') }
+        before(:all) do
+          VCR.use_cassette('search/results') do
+            @results = OMDB.search('Star Wars')
+          end
+        end
 
         it 'should return an array' do
-          results.should be_instance_of Array
+          @results.should be_instance_of Array
         end
 
         it 'should return an array with hash contents' do
-          results[0].should be_instance_of Hash
+          @results.first.should be_instance_of Hash
         end
       end
 
       describe 'with a single search result' do
-        let(:result) { OMDB.search('Star Wars: Episode IV - A New Hope') }
+        before(:all) do
+          VCR.use_cassette('search/result') do
+            @result = OMDB.search('Star Wars Episode IV A New Hope')
+          end
+        end
 
         it 'should return a hash of the title' do
-          result.should be_instance_of Hash
+          @result.should be_instance_of Hash
         end
 
         it 'should have a title' do
-          result.title.should eq('Star Wars: Episode IV - A New Hope')
+          @result.title.should eq('Star Wars: Episode IV - A New Hope')
         end
       end
 
       describe 'with no search results' do
-        let(:results) { OMDB.search('lsdfoweirjrpwef323423dsfkip') }
+        before(:all) do
+          VCR.use_cassette('search/empty') do
+            @results = OMDB.search('lsdfoweirjrpwef323423dsfkip')
+          end
+        end
 
         it 'should return a hash' do
-          results.should be_instance_of Hash
+          @results.should be_instance_of Hash
         end
 
         it 'should return a hash with a false response' do
-          results.response.should eq('False')
+          @results.response.should eq('False')
         end
 
         it 'should return a hash with an error message' do
-          results.error.should be_instance_of String
+          @results.error.should be_instance_of String
         end
       end
 
       describe 'should be aliased to find' do
         it 'should be the same method' do
-          OMDB.search('Star Wars').should eq(OMDB.find('Star Wars'))
+          VCR.use_cassette('search/find') do
+            OMDB.search('Star Wars').should eq(OMDB.find('Star Wars'))
+          end
         end
       end
     end
-
   end
-
 end
